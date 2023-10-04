f5e5c1e1ad3f3ea31b533a61660427aa563681b8
Caleb Townsend <caleb.townsend@my.jcu.edu.au>
2023-10-04 11:24:52 +1000

    Refactor variable names in subject reader program

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,13 +7,14 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = get_data()
-    print(data)
+    subjects = get_data()
+    print_data(subjects)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    subjects = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -23,7 +24,15 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        subjects.append(parts)
     input_file.close()
+    return subjects
+
+
+def print_data(subjects):
+    print(subjects)
+    for subject in subjects:
+        print(f"{subject[0]} is taught by {subject[1]} and has {subject[2]} students")
 
 
 main()
