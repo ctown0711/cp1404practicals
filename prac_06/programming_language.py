7114b1907e072e506a689afdd0ccc88abbf9a744
Caleb Townsend <caleb.townsend@my.jcu.edu.au>
2023-10-31 00:40:29 +1000

    Fix programs based on code review

---
+++ prac_06/programming_language.py
@@ -0,0 +1,24 @@
+"""
+Programming Language
+Estimate: 25 minutes
+Actual: 29 minutes
+"""
+
+
+class ProgrammingLanguage:
+    """Programming Language class"""
+
+    def __init__(self, name="", typing="", reflection="", year=0):
+        """Initialise a ProgrammingLanguage object."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        """Return string of a ProgrammingLanguage object"""
+        return f"{self.name}, {self.typing}, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        """Determine if language is dynamically typed."""
+        return self.typing == "Dynamic"
