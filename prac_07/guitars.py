302228b76b60d0b18fe025ecb29e2ddc0ed363a5
Caleb Townsend <caleb.townsend@my.jcu.edu.au>
2023-11-04 03:34:56 +1000

    Complete More Guitars program

--- prac_07/guitars.py
+++ prac_07/guitars.py
@@ -4,13 +4,22 @@ FILENAME = "guitars.csv"
 
 in_file = open(FILENAME)
 guitars = []
-in_file.readline()
 for line in in_file:
     parts = line.strip().split(',')
-    guitar = Guitar(parts[0], parts[1], float(parts[2]))
+    guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
     guitars.append(guitar)
-for guitar in guitars:
-    print(guitar)
+name = input("Name: ")
+while name != "":
+    year = int(input("Year: "))
+    cost = float(input("Cost: $"))
+    guitar = Guitar(name, year, cost)
+    guitars.append(guitar)
+    print(f"{guitar} added.")
+    name = input("Name: ")
+print("These are my guitars:")
 guitars.sort()
+out_file = open("guitar.csv", 'w')
 for guitar in guitars:
     print(guitar)
+    print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
+
