1510e09bce5f25637e18026036dadac58e5cd13f
Caleb Townsend <caleb.townsend@my.jcu.edu.au>
2023-11-17 02:07:18 +1000

    Add unreliable car class program

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -6,13 +6,16 @@ class UnreliableCar(Car):
     """Unreliable Car class"""
 
     def __init__(self, name, fuel, reliability):
+        """Initialise UnreliableCar object"""
         super().__init__(name, fuel)
         self.reliability = reliability
 
     def __str__(self):
+        """Return a string suited to the UnreliableCar object"""
         return f"{super().__str__()} Reliability: {self.reliability}"
 
     def drive(self, distance):
+        """Drive an UnreliableCar"""
         if randint(0, 100) >= self.reliability:
             distance = 0
         return super().drive(distance)
