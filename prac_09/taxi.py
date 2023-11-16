27ad3bec417535050b9b0a9fe7be1d1bc652b23d
Caleb Townsend <caleb.townsend@my.jcu.edu.au>
2023-11-17 03:36:51 +1000

    Complete silver service taxi program

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -4,11 +4,10 @@ Car class
 """
 from prac_09.car import Car
 
-price_per_km = 1.23
-
 
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs"""
+    price_per_km = 1.23
 
     def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car"""
@@ -17,11 +16,11 @@ class Taxi(Car):
 
     def __str__(self):
         """Return a string like a Car but with current fare distance"""
-        return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${price_per_km:.2f}/km"
+        return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
 
     def get_fare(self):
         """Return the price for the taxi trip"""
-        return price_per_km * self.current_fare_distance
+        return round(self.price_per_km * self.current_fare_distance, 1)
 
     def start_fare(self):
         """Begin a new fare"""
